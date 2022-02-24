49d6f26d4048f98fc80eb0056223cdc0d470ab76
kasamatsu <kasamatsu.shoji@gmail.com>
2022-02-25 07:18:51 +0900

    敵のモーションをリストに入れるテスト

---
+++ Assets/Script/GetMotionList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMotionList : MonoBehaviour
+{
+    public List<string> motionList = new List<string>();
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Animator animator = other.gameObject.GetComponent<Animator>();
+            RuntimeAnimatorController ac = animator.runtimeAnimatorController;
+            motionList.Add(ac.animationClips[0].name);
+            Debug.Log(ac.animationClips[0].name);        
+
+            Destroy(other.gameObject);
+        }
+    }
+}
