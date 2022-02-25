ee756e6990658bd71a959dfc3791c565b1de48ea
hogehoge <susumitosusyo@hotmail.com>
2022-02-25 12:37:52 +0900

    スクリプト追記　エラーはなし

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -6,29 +6,37 @@ using UnityEngine.UI;
 public class ScreenManager : MonoBehaviour
 {
     float countTime = 0;
-    public GameObject[] icons;
-
-    public void UpdateBody(int body)
-    {
-        for(int i =0;i<icons.Length;i++)
-        {
-            if (i < body) icons[i].SetActive(true);
-            else icons[i].SetActive(false);
-            
-        }
-    }
+    private static CanvasGroup canvasObj;
  
     void Start()
     {
+        canvasObj = GameObject.Find("Canvas").GetComponent<CanvasGroup>();
+        canvasObj.alpha = 0.0f;
         
+    }
+    public static void OpenPanelScript(MonoBehaviour crt,GameObject item)
+    {
+        crt.StartCoroutine(OpenEffect(item));
+    }
+    private static IEnumerator OpenEffect(GameObject item)
+    {
+        item.transform.parent = canvasObj.transform;
+        float y = canvasObj.transform.position.y + 1.0f;
+        item.transform.position = canvasObj.transform.position;
+        for (float i = 0.0f; i < 1.0f; i = i + 0.01f)
+        {
+            canvasObj.alpha = i;
+            yield return null;
+        }
+        canvasObj.alpha = 1.0f;
+      
     }
 
- 
     // Update is called once per frame
     void Update()
-    {
-        countTime += Time.deltaTime;
-        GetComponent<Text>().text = countTime.ToString("F2");
-        
-    }
+{
+    countTime += Time.deltaTime;
+    GetComponent<Text>().text = countTime.ToString("F2");
 }
+}
+   
