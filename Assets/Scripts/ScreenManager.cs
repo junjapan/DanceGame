ca6691e4655c415f0a7db7e9fc6077f7da1e84e1
hogehoge <susumitosusyo@hotmail.com>
2022-02-25 11:29:15 +0900

    script追加

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 public class ScreenManager : MonoBehaviour
 {
     float countTime = 0;
-   
+    public GameObject[] icons;
+
+    public void UpdateBody(int body)
+    {
+        for(int i =0;i<icons.Length;i++)
+        {
+            if (i < body) icons[i].SetActive(true);
+            else icons[i].SetActive(false);
+            
+        }
+    }
+ 
     void Start()
     {
         
     }
 
+ 
     // Update is called once per frame
     void Update()
     {
